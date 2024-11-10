faef009fd4682de35d2c5077272542119dce318d
rade <bajic.rade2@gmail.com>
2024-11-10 14:25:57 +0100

    observations

--- src/shaders/lorem_ipsum/fragment.glsl
+++ src/shaders/lorem_ipsum/fragment.glsl
@@ -11,32 +11,22 @@ varying vec3 vPosition;
 
 void main() {
   
-  // this pattern will not have any blue color in it
-  // here it is maybe easier to see the effect of uv coordinates
-  // because you don't have as intensive mix of colors
-  
-  // gl_FragColor = vec4(vUv, 0.0, 1.0);
 
-  // this patter is famous because you can see it in some benchmark
-  // tests, as I heard from workshop author
-  // they use it in tests for games
+  // this will produce some color mix that will be
+  // more uniform
+  // it will be yellow, that is fading from the right to the left
+  // gl_FragColor = vec4(vUv.xx, 0.0, 1.0);
 
+  // this will be yellow that is fading in oposite direction
+  // gl_FragColor = vec4(1.0 - vUv.xx, 0.0, 1.0);
 
-  // here we can play around and try to invert 
-  // direction of color mixing
-  
-  // gl_FragColor = vec4(vUv.yx, 0.0, 1.0);
-  
-  // now the red is in top left region, and
-  // green is in bottom right region
+  // this will be the same yellowish color, just fading will go
+  // from top to bottom
+  // gl_FragColor = vec4(vUv.xx, 0.0, 1.0);
+
+  // and again, same color but fading in oposite direction
+  gl_FragColor = vec4(1.0 - vUv.yy, 0.0, 1.0);
 
 
-  // but we can invert colors by substracting from 1.0
 
-  // gl_FragColor = vec4(1.0 - vUv, 0.0, 1.0);
-  // this will have same direction as we used vUv.yx
-
-
-  // but for the end let's use the one we started this lesson with
-  gl_FragColor = vec4(vUv, 0.0, 1.0);
 }
