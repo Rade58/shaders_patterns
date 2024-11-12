fa117dc751aa53f9d18d680696d06f3d4c2cbeb9
rade <bajic.rade2@gmail.com>
2024-11-12 22:38:39 +0100

    modulo

--- src/shaders/stripes/fragment.glsl
+++ src/shaders/stripes/fragment.glsl
@@ -14,9 +14,15 @@ void main() {
   // ok, we know that this, if not messed with, it will produce
   // bars pattern
   float strength = mod(vUv.y * 10.0, 1.0);
+  // we know from before that this will produce
+  // eather reminder 0.5 or 0.0 if there is no reminder
+ 
   
-  // but here we are altering the value depending on the
+  // here we are altering the value depending on the
   // reminder
+
+  // this also means we won't have gradient
+ 
   if(strength < 0.5){
     strength = 0.0; // this will create entirely black color
   }else{
