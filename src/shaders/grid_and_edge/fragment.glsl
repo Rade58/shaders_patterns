d1e811bfa41fcf8cac482f77d2fb8d3a874d64d8
rade <bajic.rade2@gmail.com>
2024-11-13 14:45:15 +0100

    grid and the edge

---
+++ src/shaders/grid_and_edge/fragment.glsl
@@ -0,0 +1,23 @@
+precision mediump float;
+// precision highp float;
+
+varying vec2 vUv;
+
+// uniform vec3 uColor;
+
+
+varying vec3 vPosition;
+
+
+void main() {
+
+  float strength = step(0.8, mod(vUv.y * 10.0, 1.0));
+  
+  // if you want to increse white spot of the grid to be more wider
+  // we will decrease edge from 0.8 to 0.2 
+  strength *= step(0.2, mod(vUv.x * 10.0, 1.0));
+
+
+  gl_FragColor = vec4(vec3(strength, strength, strength), 1.0);
+    
+}
