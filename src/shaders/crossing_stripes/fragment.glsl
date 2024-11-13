166e7695c275936917de08a6048d7b2cb40cfb55
rade <bajic.rade2@gmail.com>
2024-11-13 13:07:51 +0100

    crossstripes

--- src/shaders/crossing_stripes/fragment.glsl
+++ src/shaders/crossing_stripes/fragment.glsl
@@ -12,17 +12,19 @@ varying vec3 vPosition;
 void main() {
 
   // what can we do to also use vUv.x in our example
-  // so we can have repeating segments over x too 
-  // well we can add it to strength, just like thsi
+
 
   // instead of just using y this
   // float strength = mod(vUv.y * 10.0, 1.0);
-  // we can add a part with x
-  float strength = mod(vUv.y * 10.0, 1.0);
+  // we can define two separate strenghts
+  float strength_y = mod(vUv.y * 10.0, 1.0);
 
-  float s = mod(vUv.x * 10.0, 1.0);
+  float strength_x = mod(vUv.x * 10.0, 1.0);
 
-  strength = step(0.8, strength) + step(0.8, s);
+  // and make addition here
+  float strength = step(0.8, strength_y) + step(0.8, strength_x);
+
+  // and we will get pattern where y segments are crossing x segments
 
   gl_FragColor = vec4(vec3(strength, strength, strength), 1.0);
     
