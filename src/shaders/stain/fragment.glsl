318d1410a03f28b5a4a7062735fe9190b0f88185
rade <bajic.rade2@gmail.com>
2024-11-17 17:58:48 +0100

    stain

---
+++ src/shaders/stain/fragment.glsl
@@ -0,0 +1,47 @@
+// already defined with ShaderMaterial
+// precision mediump float;
+
+
+#define PI 3.1415926535897932384626433832795
+
+varying vec2 vUv;
+
+
+// -----------------------------------------------------------
+// -----------------------------------------------------------
+// defined those two earlier (just keeping them here if we would need
+// them in future, but they are not part of this lesson)
+// copied fron https://thebookofshaders.com/10
+float random (vec2 st) {
+  return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+// this is rotate function we copied from somewhere (used in some previous example)
+vec2 rotate(vec2 uv, float rotation, vec2 mid){
+  return vec2(
+    cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+    cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+  );
+}
+// ------------------------------------------------------------
+// ------------------------------------------------------------
+
+
+
+void main() {
+
+
+  // we added some things to vUv.x also as you can see
+  // almost the same thing we added to thr y part
+  vec2 wavedUv = vec2(
+    vUv.x + sin(vUv.y * 30.0) * 0.1, 
+    vUv.y + sin(vUv.x * 30.0) * 0.1
+  );
+
+  // instead of original vUv we use one we changed
+  float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
+
+
+  gl_FragColor = vec4(vec3(strength, strength, strength), 1.0);
+
+}
